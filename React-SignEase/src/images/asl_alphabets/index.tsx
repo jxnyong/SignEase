665ad7a3df1526cd73d537077d39245eec7ae8c4
jxnyong <131933449+jxnyong@users.noreply.github.com>
2023-07-25 15:20:19 +0800

    Added folder for ASL alphabet images

---
+++ React-SignEase/src/images/asl_alphabets/index.tsx
@@ -0,0 +1,29 @@
+// asl_alphabets/index.ts
+export { default as A } from './A.png';
+export { default as B } from './B.png';
+export { default as C } from './C.png';
+export { default as D } from './D.png';
+export { default as E } from './E.png';
+export { default as F } from './F.png';
+export { default as G } from './G.png';
+export { default as H } from './H.png';
+export { default as I } from './I.png';
+export { default as J } from './J.png';
+export { default as K } from './K.png';
+export { default as L } from './L.png';
+export { default as M } from './M.png';
+export { default as N } from './N.png';
+export { default as O } from './O.png';
+export { default as P } from './P.png';
+export { default as Q } from './Q.png';
+export { default as R } from './R.png';
+export { default as S } from './S.png';
+export { default as T } from './T.png';
+export { default as U } from './U.png';
+export { default as V } from './V.png';
+export { default as W } from './W.png';
+export { default as X } from './X.png';
+export { default as Y } from './Y.png';
+export { default as Z } from './Z.png';
+
+
